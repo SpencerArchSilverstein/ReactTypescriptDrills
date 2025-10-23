91f507461b907c73ceef4090ec36faaabccd1c36
SpencerArchSilverstein <spencerarchsilverstein@gmail.com>
2025-10-23 14:47:59 -0400

    (feat): To-Do List can now be checked off

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,50 @@
 import "./App.css";
 import TicTacToe from "./TicTacToe";
 import { useState } from "react";
+
+type ToDoList = {
+  id: number;
+  item: string;
+  checked: boolean;
+};
 function App() {
-  const [toDo, setToDo] = useState([
+  const [toDo, setToDo] = useState<Array<ToDoList>>([
     { id: 0, item: "Do laundry", checked: false },
     { id: 1, item: "Make bed", checked: false },
     { id: 2, item: "Wash dishes", checked: false },
   ]);
 
-  // function handleCheckbox(id:number) {
-  //   const newList = toDo.map((item,index) => (id{ ...toDo, checked: !item.checked });
-  //   setToDo(newList);
-  // }
+  const handleCheckbox = (id: number) => {
+    const newList = toDo.map((todoItem) => {
+      if (todoItem.id == id) {
+        console.log("Catching");
+        return { ...todoItem, checked: !todoItem.checked };
+      }
+      return todoItem;
+    });
+    // console.log(newList);
+    setToDo(newList);
+  };
   return (
     <>
-      {/* {toDo.map((todo, index) => (
+      {toDo.map((todo, index) => (
+        
         <div className="flex gap-3">
-          <input type="checkbox" onClick={() => handleCheckbox(todo)}></input>
-          <ul id={String(index)} className={todo.checked ? "line-through" : ""}>
+          <input
+            type="checkbox"
+            onClick={() => handleCheckbox(index)}
+            className="w-5"
+          ></input>
+          <ul
+            key={index}
+            className={
+              todo.checked ? "line-through bold text-3xl" : "bold text-3xl"
+            }
+          >
             {todo.item}
           </ul>
         </div>
-      ))} */}
+      ))}
       <TicTacToe />
     </>
   );
